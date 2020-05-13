6478a685056f43df4a143cbb2cbeee05de4eee6c
Tommy McHugh <tombmch@gmail.com>
2020-05-13 02:09:25 -0500

    setup basic flow tooling

--- core/v11.js
+++ core/v11.js
@@ -1,3 +1,10 @@
-const larkin = require('../bazel-bin/larkin/client/node/build/Release/larkin');
+// @flow
 
-console.log(larkin.speech.getVoices());
+/*
+ * Copyright (c) Northwestern Inclusive Technology Lab
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const larkin = require("../bazel-bin/larkin/client/node/build/Release/larkin");
